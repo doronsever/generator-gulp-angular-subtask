7d947887bbd3cfd8d58c885133627ba7fca9a32b
Doron Sever <doronsever@gmail.com>
2014-12-01 09:48:02 +0200

    created the filter sub task

---
+++ filter/index.js
@@ -0,0 +1,14 @@
+var ScriptBase = require('../app/script-base.js');
+
+module.exports = ScriptBase.extend({
+  constructor: function () {
+    ScriptBase.apply(this, arguments);
+    this.option('coffee', {
+      desc: 'Generate CoffeeScript instead of JavaScript'
+    }); // This method adds support for a `--coffee` flag
+  },
+
+  exec: function () {
+    this.appTemplate({type: 'filter'});
+  }
+});
